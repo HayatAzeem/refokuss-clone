c735809dcbe347103f7094f388bb40a22ea7e305
Hayat Azeem <hayatazeem5oct2004@gmail.com>
2025-08-02 16:50:37 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import Navbar from './components/Navbar.jsx';
-import Work from './components/Work.jsx';
-import Stripes from './components/Stripes.jsx';
+import React from "react"
+import Navbar from "./components/navbar/Navbar"
+import Work from "./components/hero/Work"
+import Stripes from "./components/stripes/Stripes"
+import Products from "./components/products/Products"
+import Marquees from "./components/marquees/Marquees"
+import Cards from "./components/cards/Cards"
+import Footer from "./components/footer/Footer"
 
-function App() {
-  return (
-    <div className="w-full h-screen bg-zinc-900 font-['satoshi'] text-white">
-      <Navbar></Navbar>
-      <Work></Work>
-      <Stripes></Stripes>
-    </div>
-  )
+import LocomotiveScroll from "locomotive-scroll"
+
+const App = () => {
+    const locomotiveScroll = new LocomotiveScroll()
+
+    return (
+        <div className="w-full bg-zinc-900 text-white">
+            <Navbar />
+            <Work />
+            <Stripes />
+            <Products />
+            <Marquees />
+            <Cards />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
